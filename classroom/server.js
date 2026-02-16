18c64a0843a1f5d85dffa32525111ff6757bbf25
GongallaAshwitha <ashwithagongalla0@gmail.com>
2026-02-16 16:29:23 +0530

    Removed unused classroom files

--- classroom/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/",(req,res)=>{
-    res.send("HI i am a roooot");
-});
-app.listen(3000,()=>{
-    console.log("server is listing to the port");
-})
